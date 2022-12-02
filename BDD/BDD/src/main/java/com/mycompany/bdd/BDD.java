9f22242f3c0b2b66c474a64a4341577f97aedf8e
aleonardomc <96079673+aleonardomc@users.noreply.github.com>
2022-12-02 00:48:27 -0600

    Add files via upload

--- BDD/BDD/src/main/java/com/mycompany/bdd/BDD.java
+++ BDD/BDD/src/main/java/com/mycompany/bdd/BDD.java
@@ -58,30 +58,30 @@ public class BDD {
             
             if (opcion == 1){
                 try {
-                    CallableStatement consultaA = conexion1.establecerConexion1().prepareCall("{CALL consultaA}");
-                    ResultSet rConsultaA = consultaA.executeQuery();
+    
                     System.out.println("PROCEDIMIENTO A\n");
                     System.out.println("Determinar el total de las ventas de los productos con la categoría que se provea\n" +
                                        "de argumento de entrada en la consulta, para cada uno de los territorios\n" +
                                        "registrados en la base de datos.\n");
                     int contador = 0;    
-                    System.out.println("TerritoryID | Ventas Totales");
-                    System.out.println("----------- | --------------");
+                    System.out.print("Ingrese un número correspondiente a la categoria: ");
+                    Scanner categoriaEntrada = new Scanner(System.in);
+                    int categoria = categoriaEntrada.nextInt();
+                    CallableStatement consultaA = conexion1.establecerConexion1().prepareCall("{CALL consultaA (?)}");
+                    consultaA.setInt(1, categoria);
+                    ResultSet rConsultaA = consultaA.executeQuery();
+                    
+                    System.out.println("ID Territorio | Ventas Totales");
+                    System.out.println("------------- | --------------");
                     while (rConsultaA.next()){
-                        if (contador == 9)
-                            System.out.print(rConsultaA.getString("TerritoryID") + "          | ");
-                        else 
-                            System.out.print(rConsultaA.getString("TerritoryID") + "           | ");
-                        contador ++;
-                        System.out.println(rConsultaA.getString("Total_Ventas"));
+                        System.out.print(rConsultaA.getString("TerritoryID") + "\t\t ");
+                        System.out.println(rConsultaA.getString("Total_Ventas"));      
                     }
                     System.out.println("\n\n");
-              
                 } 
                 catch (SQLException ex) {
-                    Logger.getLogger(BDD.class.getName()).log(Level.SEVERE, null, ex);
+                        Logger.getLogger(BDD.class.getName()).log(Level.SEVERE, null, ex);
                 }
-               
             }
             
             else if (opcion == 2){
@@ -92,8 +92,8 @@ public class BDD {
                     System.out.println("Determinar el producto más solicitado para la región (atributo group de\n" +
                                        "salesterritory) “Noth America” y en que territorio de la región tiene mayor\n" +
                                        "demanda\n");   
-                    System.out.println("Producto              | Solicitudes | Región");
-                    System.out.println("--------------------- | ----------- | ------ ");
+                    System.out.println("Producto              | Solicitudes |    Región");
+                    System.out.println("--------------------- | ----------- | ------------- ");
                     
                     while (rConsultaA.next()){
                         System.out.print(rConsultaA.getString("Producto") + "   ");
@@ -109,8 +109,32 @@ public class BDD {
             }
             
             else if (opcion == 3){
-                System.out.println("PROCEDIMIENTO C");
-            
+                try {
+                    System.out.println("PROCEDIMIENTO C\n");
+                    System.out.println("Actualizar el stock disponible en un 5% de los productos de la categoría que se\n" +
+                                       "provea como argumento de entrada en una localidad que se provea como\n" +
+                                       "entrada en la instrucción de actualización\n"); 
+
+                    Scanner idCategoriaEntrada = new Scanner(System.in);
+                    Scanner idLocalidadEntrada = new Scanner(System.in);
+                    System.out.print("Ingrese el ID de la categoría: ");
+                    int idCategoria = idCategoriaEntrada.nextInt();
+                    System.out.print("Ingrese el ID de la localidad: ");
+                    int idLocalidad = idLocalidadEntrada.nextInt();
+                    
+                    CallableStatement consultaA = conexion1.establecerConexion1().prepareCall("{CALL consultaC (?,?)}");
+                    consultaA.setInt(1, idCategoria);
+                    consultaA.setInt(2, idLocalidad);
+                    
+                    ResultSet rConsultaA = consultaA.executeQuery();                    
+
+                    System.out.println("\n\n");
+                    JOptionPane.showMessageDialog(null, "Almacen de productos actualizados.");
+
+                } 
+                catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Error al actualizar. Verifique sus datos.");
+                }
             }
             
             else if (opcion == 4){
@@ -121,31 +145,73 @@ public class BDD {
                     System.out.println("Determinar si hay clientes que realizan ordenes en territorios diferentes al que\n" +
                                        "se encuentran"); 
                  
-                    if (rConsultaA == null)
-                                System.out.println("No existen clientes que hayan realizado pedidos en territorios distintos");
-                    //System.out.println("Producto             | Solicitudes | Región");
-                    //System.out.println("-------------------- | ----------- | ----- ");
-                    while (rConsultaA.next()){
-                   //     System.out.print(rConsultaA.getString("Producto") + "  ");
-                    //    System.out.print(rConsultaA.getString("Solicitudes") + " ");
-                     //   System.out.println(rConsultaA.getString("Region"));
-                          
-                    }
+                    JOptionPane.showMessageDialog(null, "Si existen clientes que hayan realizado pedidos en territorios distintos.");
                     System.out.println("\n\n");
               
                 } 
                 catch (SQLException ex) {
-                    Logger.getLogger(BDD.class.getName()).log(Level.SEVERE, null, ex);
+                    JOptionPane.showMessageDialog(null, "No existen clientes que hayan realizado pedidos en territorios distintos.");
                 }
             }
             
             else if (opcion == 5){
-               
+            try {
+                    System.out.println("PROCEDIMIENTO E\n");
+                    System.out.println("Actualizar la cantidad de productos de una orden que se provea como\n" +
+                                       "argumento en la instrucción de actualización.\n"); 
+                 
+                    Scanner idProductoEntrada = new Scanner(System.in);
+                    Scanner idOrdenEntrada = new Scanner(System.in);
+                    Scanner cantidadEntrada = new Scanner(System.in);
+                    System.out.print("Ingrese el ID del producto: ");
+                    int idProducto = idProductoEntrada.nextInt();
+                    System.out.print("Ingrese el ID de la orden: ");
+                    int idOrden = idOrdenEntrada.nextInt();
+                    System.out.print("Ingrese la nueva cantidad: ");
+                    int cantidad = cantidadEntrada.nextInt();
+                   
+                    CallableStatement consultaA = conexion1.establecerConexion1().prepareCall("{CALL consultaE (?,?,?)}");
+                    consultaA.setInt(1, idProducto);
+                    consultaA.setInt(2, idOrden);
+                    consultaA.setInt(3, cantidad);
+                    
+                    ResultSet rConsultaA = consultaA.executeQuery();                    
+
+                    System.out.println("\n\n");
+                    JOptionPane.showMessageDialog(null, "Cantidad actualizada.");
+
+                } 
+                catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Error al actualizar. Verifique sus datos.");
+                }   
             }
             
             else if (opcion == 6){
-                System.out.println("PROCEDIMIENTO F");
-            
+               try {
+                    System.out.println("PROCEDIMIENTO F\n");
+                    System.out.println("Actualizar el método de envío de una orden que se reciba como argumento en\n" +
+                                       "la instrucción de actualización."); 
+                 
+                    Scanner envioEntrada = new Scanner(System.in);
+                    Scanner idEntrada = new Scanner(System.in);
+                    System.out.print("Ingrese el método de envio: ");
+                    int envio = envioEntrada.nextInt();
+                    System.out.print("Ingrese el ID de orden de venta: ");
+                    int id = idEntrada.nextInt();
+                    
+                    CallableStatement consultaA = conexion1.establecerConexion1().prepareCall("{CALL consultaF (?,?)}");
+                    consultaA.setInt(1, envio);
+                    consultaA.setInt(2, id);
+                    
+                    ResultSet rConsultaA = consultaA.executeQuery();                    
+
+                    System.out.println("\n\n");
+                    JOptionPane.showMessageDialog(null, "Método de envio actualizado.");
+
+                } 
+                catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Error al actualizar. Verifique sus datos.");
+                }
             }
             
             else if (opcion == 7){
@@ -171,8 +237,8 @@ public class BDD {
                     
                     ResultSet rConsultaA = consultaA.executeQuery();
                     
+                    System.out.println("\n\n");  
                     JOptionPane.showMessageDialog(null, "Se actualizo el nuevo correo.");
-                    System.out.println("\n\n");             
                 }   
                 catch (SQLException ex) {
                     //Logger.getLogger(BDD.class.getName()).log(Level.SEVERE, null, ex);
